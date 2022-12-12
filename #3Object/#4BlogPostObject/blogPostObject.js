0f629210628173cd5f0135be6e051d455da88390
GutoAS <agostinhosoberano2@gmail.com>
2022-12-12 13:48:03 +0200

    blogPost

---
+++ #3Object/#4BlogPostObject/blogPostObject.js
@@ -0,0 +1,27 @@
+const blogPost = {
+  title: "Recipes",
+  body: "lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,",
+  author: "Agostinho",
+  views: 20,
+  comments: [
+    {
+      author: "Agostinho",
+      body: "lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,",
+    },
+    {
+      author: "Agostinho",
+      body: "lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,",
+    },
+    {
+      author: "Agostinho",
+      body: "lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,",
+    },
+    {
+      author: "Agostinho",
+      body: "lorem ipsum,lorem ipsum,lorem ipsum,lorem ipsum,",
+    },
+  ],
+  isLive: true,
+};
+
+console.log(blogPost);
