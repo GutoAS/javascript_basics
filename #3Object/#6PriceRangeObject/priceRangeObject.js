363ec362b246f343e4346957ea277c20445aff94
GutoAS <agostinhosoberano2@gmail.com>
2022-12-12 14:13:13 +0200

    priceRange

---
+++ #3Object/#6PriceRangeObject/priceRangeObject.js
@@ -0,0 +1,9 @@
+let priceRange = [
+  { label: "$", tooltip: "Inexpensive", minPrice: 0, maxPrice: 10 },
+  { label: "$$", tooltip: "Moderate", minPrice: 11, maxPrice: 20 },
+  { label: "$$$", tooltip: "Expensive", minPrice: 21, maxPrice: 50 },
+];
+
+let restaurants = {
+  averageRange: 5,
+};
