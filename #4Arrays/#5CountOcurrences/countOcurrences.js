977643b367ac2c6635a845a47b9db18e1ecb882e
GutoAS <agostinhosoberano2@gmail.com>
2022-12-13 16:02:50 +0200

    countOcurrences changed

--- #4Arrays/#5CountOcurrences/countOcurrences.js
+++ #4Arrays/#5CountOcurrences/countOcurrences.js
@@ -1,4 +1,8 @@
 function occurences(array, searchElement) {
+  if (!Array.isArray(array)) {
+    throw new Error("Invalid array");
+  }
+
   let count = 0;
   for (let element of array) {
     if (element === searchElement) count++;
@@ -6,6 +10,10 @@ function occurences(array, searchElement) {
   return count;
 }
 
-const array = [1, 2, 3, 1, 4, 1];
-
-console.log(occurences(array, 2));
+try {
+  const array = [1, 2, 3, 1, 4, 1];
+  const occurence = occurences(array, 1);
+  console.log(occurence);
+} catch (e) {
+  console.log(e.message);
+}
